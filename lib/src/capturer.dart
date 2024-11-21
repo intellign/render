4cf89cf6f30a613ccd3875001c1a390ec70a5c1d
intellign <ismailarashad@aol.com>
2024-11-21 05:17:50 -0500

    Update capturer.dart

--- lib/src/capturer.dart
+++ lib/src/capturer.dart
@@ -284,12 +284,28 @@ class RenderCapturer<K extends RenderFormat> {
         view: flutterView,
         child: RenderPositionedBox(
             alignment: Alignment.center, child: repaintBoundary),
+        configuration: RenderPositionedBox(
+            alignment: Alignment.center, child: repaintBoundary),
         configuration: ViewConfiguration(
+          size: logicalSize,
+          devicePixelRatio: session.settings.pixelRatio,
+        ),
+
+        /*
+      ViewConfiguration(
           physicalConstraints:
               BoxConstraints.tight(logicalSize) * flutterView.devicePixelRatio,
           logicalConstraints: BoxConstraints.tight(logicalSize),
           devicePixelRatio: session.settings.pixelRatio,
-        ),
+        )
+
+      ViewConfiguration(
+        physicalConstraints: BoxConstraints.tight(logicalSize) *
+            View.of(context).devicePixelRatio,
+        logicalConstraints: BoxConstraints.tight(logicalSize),
+        devicePixelRatio: View.of(context).devicePixelRatio,
+      ),
+      */
       );
 
       final PipelineOwner pipelineOwner = PipelineOwner();
